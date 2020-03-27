87d30ed5d1359bb3fdc6cf4ad34273bff323bd27
Memo Khoury <majd.khoury93@gmail.com>
2020-03-27 15:52:55 -0400

    pkg: added mercarator
    cp: attempted rendering of grid

--- virotesttwo/js/HelloWorldSceneAR.js
+++ virotesttwo/js/HelloWorldSceneAR.js
@@ -1,10 +1,13 @@
-'use strict';
+"use strict";
 
 // we import react in the beginning. Component is also imported because this is a class-based component.
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // next we import StyleSheet from react .. pretty self-explanatory
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+
+// we import mercator as merc. this will allow us to convert geolocated coordinates into AR coordinates
+var merc = require("mercator-projection");
 
 // Necessary components from react-viro that are required to construct this scene
 import {
@@ -18,13 +21,13 @@ import {
   ViroSpotLight,
   ViroAnimations,
   ViroARPlaneSelector,
-  ViroNode
-} from 'react-viro';
+  ViroNode,
+  ViroPolyline
+} from "react-viro";
 
 let interval;
 export default class HelloWorldSceneAR extends Component {
-
-  // In a react component life-cycle, the constructor is called first. 
+  // In a react component life-cycle, the constructor is called first.
   // The two purposes for the instructor is initialize the local state and/or binding an event handler(s) into an instance.
   constructor() {
     // super() is the parent constructor. Usually, you pass props inside the super() function
@@ -35,7 +38,11 @@ export default class HelloWorldSceneAR extends Component {
       animation: "loopRotate",
       threeWords: "///searching.for.words",
       position: "",
-      incrementor: 1
+      latitude: "",
+      longitude: "",
+      devicePosition: {},
+      incrementor: 1,
+      grid: "no grid yet"
     };
 
     // bind 'this' to functions
@@ -53,79 +60,175 @@ export default class HelloWorldSceneAR extends Component {
     this.setState({ incrementor: 0 });
   }
 
+  findCoordinates = () => {
+    // this.setState({ incrementor: this.state.incrementor + 1 });
+    navigator.geolocation.watchPosition(
+      position => {
+        const location = JSON.stringify(position);
+        var devicePosition = merc.fromLatLngToPoint({
+          lat: Number(position.coords.latitude),
+          lng: Number(position.coords.latitude)
+        });
+        // this.setState({ incrementor: this.state.incrementor + 1 });
+        this.setState({
+          position: position.coords.latitude + ", " + position.coords.longitude,
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+          devicePosition: devicePosition
+        });
+        this.get3Words(position.coords.latitude, position.coords.longitude);
+        this.getGrid(position.coords.latitude, position.coords.longitude);
+      },
+      error => Alert.alert(error.message)
+    );
+  };
+
   get3Words = (lat, lng) => {
     var textURL = `https://api.what3words.com/v3/convert-to-3wa?coordinates=${lat},${lng}&language=en&key=UAT9QR8X`;
     fetch(textURL, {
       method: "GET"
-    }).then(res => { res.json().then(result => {
-        this.setState({
-          threeWords: "///" + result.words
+    }).then(res => {
+      res
+        .json()
+        .then(result => {
+          this.setState({
+            threeWords: "///" + result.words
+          });
         })
-      })
-      .catch(e => console.error("Oops! Something is going on:  " + e));
-    })
-  }
+        .catch(e => console.error("Oops! Something is going on:  " + e));
+    });
+  };
 
-  findCoordinates = () => {
-    // this.setState({ incrementor: this.state.incrementor + 1 });
-      navigator.geolocation.watchPosition(
-        position => {
-          const location = JSON.stringify(position);
-          this.setState({ incrementor: this.state.incrementor + 1});
-          this.setState({position: position.coords.latitude + ", " + position.coords.longitude});
-          this.get3Words(position.coords.latitude, position.coords.longitude);
-        },
-        error => Alert.alert(error.message),
-      );
-  }
+  getGrid = (lat, lng) => {
+    var southWestLat = lat - 0.001;
+    var southWestLng = lng - 0.001;
+    var northEastLat = lat + 0.001;
+    var northEastLng = lng + 0.001;
+    var textUrl = `https://api.what3words.com/v3/grid-section?key=UAT9QR8X&bounding-box=${southWestLat}%2C${southWestLng}%2C${northEastLat}%2C${northEastLng}&format=json`;
+    fetch(textUrl, {
+      method: "Get"
+    })
+      .then(res => {
+        res.json().then(result => {
+          this.renderGrid(result);
+          // this.setState({ grid: JSON.stringify(result) });
+        });
+      })
+      .catch(e => {
+        this.setState({ grid: e.toString() });
+      });
+  };
+
+  renderGrid = (result) => {
+    var gridList = [];
+    for (let element of result.lines) {
+      var startPoint = merc.fromLatLngToPoint({
+        lat: element.start.lat,
+        lng: element.start.lng
+      });
+      var endPoint = merc.fromLatLngToPoint({
+        lat: element.end.lat,
+        lng: element.end.lng
+      });
+      var startPositionX = startPoint.x - this.state.devicePosition.x;
+      var startPositionZ = startPoint.y - this.state.devicePosition.y;
+      var endPositionX = endPoint.x - this.state.devicePosition.x;
+      var endPositionZ = endPoint.y - this.state.devicePosition.y;
+      var startPosition = { x: startPositionX, z: startPositionZ };
+      var endPosition = { x: endPositionX, z: endPositionZ };
+      var position = { start: startPosition, end: endPosition };
+      gridList.push(position);
+    };
+    console.log(gridList);
+    this.grid = gridList.map(element => (
+      <ViroPolyline
+        key={start.x}
+        points={[
+          [element.start.x, 0, element.start.y],
+          [element.end.x, 0, element.end.y]
+        ]}
+      ></ViroPolyline>
+    ));
+  };
 
   // In the next part of the component life-cycle, we render() the component. In this case, we are simply returning the ARScene with a child
   // ViroText that contains the initial state text `Initializing AR....`.
   render() {
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.threeWords} scale={[1, 1, 1]} position={[.3, 1, -3.0]} style={styles.helloWorldTextStyle} />
-        <ViroText text={this.state.position} scale={[1, 1, 1]} position={[.3, 0.5, -3.0]} style={styles.helloWorldTextStyle} />
-        <ViroText text={this.state.incrementor.toString()} scale={[1, 1, 1]} position={[.3, 0, -3.0]} style={styles.helloWorldTextStyle} />
+      <ViroARScene onTrackingUpdated={this._onInitialized}>
+        {this.grid}
+        <ViroText
+          text={this.state.threeWords}
+          scale={[1, 1, 1]}
+          position={[0.3, 1, -3.0]}
+          style={styles.helloWorldTextStyle}
+        />
+        <ViroText
+          text={this.state.position}
+          scale={[1, 1, 1]}
+          position={[0.3, 0.5, -3.0]}
+          style={styles.helloWorldTextStyle}
+        />
+        <ViroText
+          text={this.state.incrementor.toString()}
+          scale={[1, 1, 1]}
+          position={[0.3, 0, -3.0]}
+          style={styles.helloWorldTextStyle}
+        />
+        <ViroText
+          text={this.state.grid}
+          scale={[1, 1, 1]}
+          position={[-0.6, 0, -1.0]}
+          style={styles.helloWorldTextStyle}
+        />
         <ViroAmbientLight color={"#aaaaaa"} />
-        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]}
-          position={[0, 3, -1]} color="#ffffff" castsShadow={true} />
-          <ViroARPlaneSelector>
+        <ViroSpotLight
+          innerAngle={5}
+          outerAngle={90}
+          direction={[0, -1, -0.2]}
+          position={[0, 3, -1]}
+          color="#ffffff"
+          castsShadow={true}
+        />
+        <ViroARPlaneSelector>
           <Viro3DObject
-              source={require('./res/LTGMASK_3dmodel.obj')}
-              resources={[require('./res/LTGMASK_3dmodel.obj'),
-                  require('./res/LTGMASK_3dmodel.obj'),
-                  require('./res/LTGMASK_3dmodel.obj')]}
-              animation={{name:'loopRotate',
-                  run:true,
-                  loop:true}}
-              position={[-3, .1, -1]}
-              scale={[.002, .002, .002]}
-              type="OBJ" />
-          </ViroARPlaneSelector>
+            source={require("./res/LTGMASK_3dmodel.obj")}
+            resources={[
+              require("./res/LTGMASK_3dmodel.obj"),
+              require("./res/LTGMASK_3dmodel.obj"),
+              require("./res/LTGMASK_3dmodel.obj")
+            ]}
+            animation={{ name: "loopRotate", run: true, loop: true }}
+            position={[-3, 0.1, -1]}
+            scale={[0.002, 0.002, 0.002]}
+            type="OBJ"
+          />
+        </ViroARPlaneSelector>
         <Viro3DObject
-              source={require('./res/LTGMASK_3dmodel.obj')}
-              resources={[require('./res/LTGMASK_3dmodel.obj'),
-                  require('./res/LTGMASK_3dmodel.obj'),
-                  require('./res/LTGMASK_3dmodel.obj')]}
-              animation={{name:'loopRotate',
-                  run:true,
-                  loop:true}}
-              position={[0, 0, -3]}
-              scale={[.002, .002, .002]}
-              type="OBJ" />
+          source={require("./res/LTGMASK_3dmodel.obj")}
+          resources={[
+            require("./res/LTGMASK_3dmodel.obj"),
+            require("./res/LTGMASK_3dmodel.obj"),
+            require("./res/LTGMASK_3dmodel.obj")
+          ]}
+          animation={{ name: "loopRotate", run: true, loop: true }}
+          position={[0, 0, -3]}
+          scale={[0.002, 0.002, 0.002]}
+          type="OBJ"
+        />
         <ViroNode>
           <Viro3DObject
-                source={require('./res/emoji_smile/emoji_smile.vrx')}
-                resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
-                    require('./res/emoji_smile/emoji_smile_normal.png'),
-                    require('./res/emoji_smile/emoji_smile_specular.png')]}
-                animation={{name: this.state.animation,
-                    run:true,
-                    loop:true}}
-                position={[-.5, .1, -3]}
-                scale={[.6, .6, .6]}
-                type="VRX" />
+            source={require("./res/emoji_smile/emoji_smile.vrx")}
+            resources={[
+              require("./res/emoji_smile/emoji_smile_diffuse.png"),
+              require("./res/emoji_smile/emoji_smile_normal.png"),
+              require("./res/emoji_smile/emoji_smile_specular.png")
+            ]}
+            animation={{ name: this.state.animation, run: true, loop: true }}
+            position={[-0.5, 0.1, -3]}
+            scale={[0.6, 0.6, 0.6]}
+            type="VRX"
+          />
         </ViroNode>
       </ViroARScene>
     );
@@ -136,14 +239,14 @@ export default class HelloWorldSceneAR extends Component {
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "LONO THE GOD"
+        text: "LONO THE GOD"
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
   }
 
-  _onDrag(dragToPos, source)  {
+  _onDrag(dragToPos, source) {
     // this.setState({
     //   text: "dragging emoji!"
     // });
@@ -153,28 +256,28 @@ export default class HelloWorldSceneAR extends Component {
   }
 }
 
-// We declare the styles here. Self-explanatory... 
+// We declare the styles here. Self-explanatory...
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
-    fontFamily: 'Courier New',
+    fontFamily: "Courier New",
     fontSize: 20,
-    color: '#e11f26',
-    fontWeight: 'bold',
-    textAlignVertical: 'center',
-    textAlign: 'center',  
-  },
+    color: "#e11f26",
+    fontWeight: "bold",
+    textAlignVertical: "center",
+    textAlign: "center"
+  }
 });
 
 // We define the 'grid' value here. The require() function is a special function which converts the filepath into a value to be used, so the platform can fetch and use this resource.
 ViroMaterials.createMaterials({
   grid: {
-    diffuseTexture: require('./res/grid_bg.jpg'),
+    diffuseTexture: require("./res/grid_bg.jpg")
   }
 });
 
 // Here we declare an animation that loops on rotate. It rotates around the Y axis, which is what we want!
 ViroAnimations.registerAnimations({
-  loopRotate:{properties:{rotateY:"+=45"}, duration:500},
+  loopRotate: { properties: { rotateY: "+=45" }, duration: 500 }
 });
 
 module.exports = HelloWorldSceneAR;
